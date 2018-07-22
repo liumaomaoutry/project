0269c206945a78cad461d9bae83e15076f2320db
liumaomaoutry <liumaomao@utry.cn>
2018-07-22 19:23:18 +0800

    Revert "自定义组件注解"
    This reverts commit 8b44b76

--- src/main/java/com/example/springapplication/RepositoryDemo.java
+++
@@ -1,7 +0,0 @@
-package com.example.springapplication;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class RepositoryDemo {
-}
--- src/main/java/com/example/springapplication/ServiceDemo.java
+++
@@ -1,7 +0,0 @@
-package com.example.springapplication;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class ServiceDemo {
-}
